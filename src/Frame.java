a3f5827a52409403db7c6121c4804de294e524b4
NQKhanh296 <khanhvn296@gmail.com>
2024-05-05 18:34:45 +0200

    Class Frame updated, added new method paintComponent() in class Game

--- src/Frame.java
+++ src/Frame.java
@@ -1,3 +1,21 @@
+import javax.swing.*;
+
 public class Frame {
+    public Frame(){
+        JFrame jFrame = new JFrame("Flappy Bird");
+        Game game = new Game();
+        jFrame.setSize(game.getWidth(), game.getHeight());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.add(game);
+        jFrame.pack();
+        game.requestFocus();
+        jFrame.setVisible(true);
+
+
+
+    }
+
 
 }
--- src/Game.java
+++ src/Game.java
@@ -5,20 +5,26 @@ import java.util.ArrayList;
 public class Game extends JPanel {
     private int width;
     private int height;
+    private Image background;
     private Image bottomPipe;
     private Image topPipe;
     private ArrayList<Pipe> pipes;
+    private Bird bird;
     public Game() {
         width = 360;
         height = 640;
-        pipes = new ArrayList<>();
         setPreferredSize(new Dimension(width, height));
+        pipes = new ArrayList<>();
+        background = new ImageIcon(getClass().getResource("flappybirdbg.png")).getImage();
         bottomPipe = new ImageIcon(getClass().getResource("bottompipe.png")).getImage();
         topPipe = new ImageIcon(getClass().getResource("toppipe.png")).getImage();
+        bird = new Bird();
     }
 
-
-
-
-
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background,0,0,width,height,null);
+        g.drawImage(bird.getBirdImage(),bird.getX(), bird.getY(),bird.getBirdWidth(),bird.getBirdHeight(),null);
+    }
 }
